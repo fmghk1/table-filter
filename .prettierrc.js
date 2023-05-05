49fa971b71af88bced538a728d6c6ca9716eac54
fmghk1 <hazel.kim@fmg.co.nz>
2023-05-05 12:26:25 +1200

    Update to get basic partitioning work

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  jsonDoubleQuote: true,
+  singleQuote: true,
+  printWidth: 140,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+};
--- src/makeData.ts
+++ src/makeData.ts
@@ -1,48 +1,45 @@
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
 
-export type Person = {
-  firstName: string
-  lastName: string
-  age: number
-  visits: number
-  progress: number
-  status: 'relationship' | 'complicated' | 'single'
-  subRows?: Person[]
-}
+export type ItemSummary = {
+  description: string;
+  coverType: string;
+  sumInsured: string;
+  excess: string;
+  address: string;
+};
 
 const range = (len: number) => {
-  const arr = []
+  const arr = [];
   for (let i = 0; i < len; i++) {
-    arr.push(i)
+    arr.push(i);
   }
-  return arr
-}
+  return arr;
+};
 
-const newPerson = (): Person => {
+const newPerson = (): ItemSummary => {
   return {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.datatype.number(40),
-    visits: faker.datatype.number(1000),
-    progress: faker.datatype.number(100),
-    status: faker.helpers.shuffle<Person['status']>([
-      'relationship',
-      'complicated',
-      'single',
-    ])[0]!,
-  }
+    description: faker.vehicle.vehicle(),
+    coverType: 'Present Day Value',
+    sumInsured: String(faker.datatype.number(10000)),
+    excess: String(faker.datatype.number(1000)),
+    address: pickRandomValueFromArray(['203 Prince Manors Suite 130', '822 Roob Lights Suite 468', '2743 Considine Spring Apt. 823']),
+  };
+};
+
+function pickRandomValueFromArray<T>(arr: T[]): T {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 export function makeData(...lens: number[]) {
-  const makeDataLevel = (depth = 0): Person[] => {
-    const len = lens[depth]!
-    return range(len).map((d): Person => {
+  const makeDataLevel = (depth = 0): ItemSummary[] => {
+    const len = lens[depth]!;
+    return range(len).map((d): ItemSummary => {
       return {
         ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-      }
-    })
-  }
+      };
+    });
+  };
 
-  return makeDataLevel()
+  return makeDataLevel();
 }
